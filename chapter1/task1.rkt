0faa182c8487d7e076b87c67feac1d26be1334ea
OGalloy <56289594+OGalloy@users.noreply.github.com>
2023-03-09 11:57:01 +0500

    Update task1.rkt

--- chapter1/task1.rkt
+++ chapter1/task1.rkt
@@ -1,4 +1,6 @@
 #lang racket
+(println "Chapter 1.")
+(println "Task1.1")
 ;;task1.1
 (equal? 10 10)
 (equal? (+ 5 3 4) 12)
@@ -31,3 +33,17 @@
 (equal? (* (cond ((> a b) a)
                  ((< a b) b)
                  (else  -1)) (+ a 1)) 16)
+
+;;task1.2
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+   (* 3 (- 6 2) (- 2 7)))
+
+;;task1.3
+(define (square x) (* x x))
+(define (sum-of-squares x y) (+ (square x) (square y)))
+(define (max-sum-of-squares a b c)
+        (cond ((and (> a c) (> b c)) (sum-of-squares a b))
+              ((and (> b a) (> c a)) (sum-of-squares b c))
+              (else (sum-of-squares a c))))
+(println "Task1.3")
+(max-sum-of-squares -1 3 3)
